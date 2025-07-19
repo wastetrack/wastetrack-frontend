4df5ca9ff2e05cc9d402803702553b48952884e0
Hilmi Fawwaz Sa'ad <128681954+hilmifawwazsaad@users.noreply.github.com>
2025-07-19 23:55:38 +0700

    feat: add interfaces for waste transfer requests and offerings

---
+++ types/waste-transfer-request-item.ts
@@ -0,0 +1,28 @@
+// types/waste-transfer-item-offerings.ts
+import { PagingInfo } from './waste-drop-request-item';
+
+export interface WasteTransferItemOffering {
+  id: string;
+  transfer_form_id: string;
+  waste_type_id: string;
+  offering_weight: number;
+  offering_price_per_kgs: number;
+  accepted_weight: number;
+  accepted_price_per_kgs: number;
+}
+
+export interface WasteTransferItemOfferingsListParams {
+  transfer_form_id?: string;
+  waste_type_id?: string;
+  page?: number;
+  size?: number;
+}
+
+export interface WasteTransferItemOfferingsListResponse {
+  data: WasteTransferItemOffering[];
+  paging: PagingInfo;
+}
+
+export interface WasteTransferItemOfferingsDetailResponse {
+  data: WasteTransferItemOffering;
+}
